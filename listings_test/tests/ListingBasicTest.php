b2c9f89cecd4f783cd25f65b73a259abeb8cd3da
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-06 22:47:07 -0500

    Update ListingBasicTest.php
    Add code structure to class

---
+++ listings_test/tests/ListingBasicTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+class ListingBasicTest extends TestCase 
+{ // Write tests for the ListingBasic class to ensure that 
+        // all three Exception messages are returned as expected.
+    
+    /** @test */
+    function exceptionMessagesReturnedAsExpected() {
+
+    }
+}
