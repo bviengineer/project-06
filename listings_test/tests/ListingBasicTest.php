4736b46e7901d48c254233e76a2aa478102c4c02
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-07 23:58:19 -0500

    tests/ListingBasicTest.php > invalid ID test
    Add test to check for exception in the event of an invalid ID

--- listings_test/tests/ListingBasicTest.php
+++ listings_test/tests/ListingBasicTest.php
@@ -1,16 +1,25 @@
 <?php
+// Write tests for the ListingBasic class to ensure that all three Exception messages are returned as expected.
 
 use PHPUnit\Framework\TestCase;
 
 class ListingBasicTest extends TestCase 
-{ // Write tests for the ListingBasic class to ensure that 
-        // all three Exception messages are returned as expected.
-    
+{ 
     /** @test */
-    function listingCannotBeCreatedDataIsUnavailable() {
+    function listingCannotBeCreatedDataIsUnavailable() 
+    {
         $this->expectExceptionMessage('Unable to create a listing, data unavailable');
        
         new ListingBasic();
         //$listingBasic->setValues('Unable to create a listing, data unavailable');
+    }
+    
+    /** @test */
+    function invalidIdCannotCreateListing() 
+    {
+        $this->expectExceptionMessage('Unable to create a listing, invalid id');
+
+        $newListing = new ListingBasic();
+        $newListing->setId($data['id']);
     }
 }
