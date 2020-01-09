6ac0fb9efab43ceef61d707cad7241a0ce0328e9
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-09 13:18:50 -0500

    tests/ListingPremiumTest.php
    Write test for getting the status of the Premium class

--- listings_test/tests/ListingPremiumTest.php
+++ listings_test/tests/ListingPremiumTest.php
@@ -1,2 +1,23 @@
 <?php
 // Write a test for the ListingPremium class to ensure that getStatus method returns 'premium'.
+
+use PHPUnit\Framework\TestCase;
+
+class ListingBasicTest extends TestCase 
+{
+      /** @test */
+      function getStatusMethodReturnsPremium()
+      {
+          // Data
+          $data = [
+              'id' => 1,
+              'title' => 'Verify the status for this premium class'
+          ];
+  
+          // Instance of ListingPremium
+          $returnStatus = new ListingPremium($data);
+  
+          // Test
+          $this->assertEquals('premium', $returnStatus->getStatus());
+     }
+}
