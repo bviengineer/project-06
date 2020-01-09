85528345c767f395576e3f9a34261808ce73ed4f
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-09 14:12:22 -0500

    tests/ListingFeaturedTest.php > getStatus()
    Add test code to verify that the featured status is returned

--- listings_test/tests/ListingFeaturedTest.php
+++ listings_test/tests/ListingFeaturedTest.php
@@ -5,5 +5,22 @@ use PHPUnit\Framework\TestCase;
 class ListingFeaturedTest extends TestCase 
 {
 // Write a test for the ListingFeatured class to ensure that getStatus method returns 'featured'.
+    /** @test */
+    function getStatusMethodReturnsFeatured()
+    {
+        {
+            // The Data
+            $data = [
+                'id' => 1,
+                'title' => 'Verify that the status for this is featured'
+            ];
+    
+            // Instance of ListingFeatured
+            $returnStatus = new ListingFeatured($data);
+    
+            // Test
+            $this->assertEquals('featured', $returnStatus->getStatus());
+       }
+    }
 
 }
