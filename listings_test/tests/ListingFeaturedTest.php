ec22796dc8910dc65541de3ce3db68cf4ca9ba05
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-09 14:06:12 -0500

    Create ListingFeaturedTest.php
    Create new file for tests for ListingFeatured class

---
+++ listings_test/tests/ListingFeaturedTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ListingFeaturedTest extends TestCase 
+{
+// Write a test for the ListingFeatured class to ensure that getStatus method returns 'featured'.
+
+}
